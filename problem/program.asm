24d1ff978103fee47446c74b57b72ecf1e96e646
Kayre <kayre@pop-os.localdomain>
2022-09-03 12:00:11 -0500

    Cross Program on ASM add

--- problem/program.asm
+++ problem/program.asm
@@ -1,4 +1,154 @@
-// COLOCA AQUÍ TU NOMBRE COMPLETO
-// COLOCA AQUÍ TU ID
-// COLOCA AQUÍ TU CORREO ELECTRÓNICO
+// William Alejandro pabon Guerrero
+// 225366
+// william.pabon@upb.edu.co
 
+(INIT)
+        @KBD
+        D=M
+        @84
+        D=D-A
+
+        @SETLINEV       //Vertical line set up
+        D;JEQ
+        //Cinfigure T input
+
+        @KBD
+        D=M
+        @67
+        D=D-A
+
+        @SETLCL       //Clean screen set up
+        D;JEQ
+        //Cinfigure C input
+
+
+        @INIT
+        D;JMP
+
+
+
+(SETCL)
+        @iscroos
+        M=1
+
+        @color
+        M=0
+
+        @16384          //16384 + (0*32) + (0/16)
+        D=A
+        @coord          //Variable that save start of the screen memory 
+        M=D
+
+        @8192           //register amount
+        D=A
+        @cont           //files counter
+        M=D
+
+        @1
+        D=A 
+        @salto          //jump
+        M=D
+
+        @RECT
+        0;JMP
+
+
+
+(SETLINEH)
+        @iscroos
+        M=1
+
+        @color
+        M=-1
+
+        @20480          //16384 + (128*32) + (0*16)
+        D=A
+        @coord          //Variable that save start of the screen memory 
+        M=D
+
+        @32             //register amount
+        D=A
+        @cont           //files counter
+        M=D
+
+        @1
+        D=A
+        @jump           //jump
+        M=D
+
+        @RECT
+        0;JMP
+
+
+
+(SETLINEV)
+        @iscroos
+        M=0
+
+        @256
+        D=A
+        @color
+        M=D
+
+        @16400          //16384 + (0*32) + (256/16)
+        D=A
+        @coord          //Variable that save start of the screen memory 
+        M=D  
+
+        @256            //register amount
+        D=A
+        @cont           //files counter
+        M=D
+
+        @32             //jump
+        D=A
+        @jump
+        M=D
+
+        @RECT
+        0;JMP
+
+
+
+(RECT)
+        @coord 
+        D=M
+        @pscreen
+        M=D
+
+
+
+(PAINT)
+        @cont           
+        D=M
+
+        @END
+        D;JEQ           //if cont = 0 go to SETLINEH
+
+        //-----------------------------------------------------
+
+        @color
+        D=M
+        @pscreen
+        A=M
+        M=D             //Paint Black
+        
+        @cont           //cont-1
+        M=M-1
+
+        @jump
+        D=M
+
+        @pscreen
+        M=M+D
+
+        @PAINT
+        0;JMP
+
+(END)
+        @iscroos
+        D=M
+        @SETLINEH
+        D;JEQ
+        @INIT
+        0;JMP
